e6701ae1d677c1552cb80840882b645288450315
Charles Manuel Diestro <charlesdiestro6@gmail.com>
2025-01-01 13:59:26 +0800

    Enhance build script and Vercel configuration: added Composer installation to build.sh for dependency management, and updated vercel.json to use the new build script for improved deployment process.

--- build.sh
+++ build.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Install composer
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php composer-setup.php --install-dir=/usr/local/bin --filename=composer
+php -r "unlink('composer-setup.php');"
+
 # Install dependencies
 composer install --no-dev --optimize-autoloader
 npm install
